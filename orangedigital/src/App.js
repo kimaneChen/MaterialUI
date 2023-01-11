eca4f913a5bf4dd1293c0db6c60ec51aa4b7d44c
Kimane <chenjx1024@outlook.com>
2023-01-11 10:41:51 +1000

    build the route

--- orangedigital/src/App.js
+++ orangedigital/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Gallery from './pages/gallery';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <div>
+      <header>Header</header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Gallery />} />
+          <Route path="detailpage" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+      <div className="App">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </div>
     </div>
   );
 }
